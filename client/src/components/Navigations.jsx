f16a672d4c9b1b938840e2806ce4fa72d19725db
bekah <bekahritter@gmail.com>
2024-07-01 16:54:04 -0700

    updated links in navbar

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -28,25 +28,27 @@ export default function Navigations({ somm, setSomm }) {
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">Paso</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/Account">Account</Nav.Link>
+              {!somm ? (
+                <NavDropdown title="ADMIN" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/AllUsers">Somms</NavDropdown.Item>
+                  <NavDropdown.Item href="/AdminWineries">
+                    Wineries
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="/AdminReviews">
+                    Reviews
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : null}
             </Nav>
           </Navbar.Collapse>
         </Container>
