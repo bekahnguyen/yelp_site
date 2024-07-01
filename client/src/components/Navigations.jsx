a2e30f51aa3bad8b6e281bf0dd5163c7db346f92
bekah <bekahritter@gmail.com>
2024-07-01 16:50:01 -0700

    ok?

--- client/src/components/Navigations.jsx
+++ client/src/components/Navigations.jsx
@@ -2,6 +2,10 @@ import { useState } from "react";
 import React from "react";
 import { Link, Navigate } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 //show edit or delete button on regular customer page, conditionally render if "is admin?"
 //or
@@ -22,31 +26,31 @@ export default function Navigations({ somm, setSomm }) {
 
   return (
     <>
-      <nav className="navbar">
-        <div className="navbar-container">
-          <Link to="/">Peruse</Link>
-          <Link to="/Account">Account</Link>
-          {!somm ? (
-            <Link to="/Login">Login</Link>
-          ) : (
-            <button onClick={handleLogout}>Logout.</button>
-          )}
-          {somm.is_admin ? (
-            <>
-              {" "}
-              <select
-                onChange={(event) => handleSubmit(event.target.value)}
-                defaultValue=""
-              >
-                <option value=" disabled">ADMIN:</option>
-                <option value="/AllUsers">Users</option>
-                <option value="/AdminReviews">Reviews</option>
-                <option value="/AdminWineries">Wineries</option>
-              </select>
-            </>
-          ) : null}
-        </div>
-      </nav>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
