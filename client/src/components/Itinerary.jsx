6a4b0bbf6342de0f66ee494d5faa62d4fd0ea894
bekah <bekahritter@gmail.com>
2024-08-27 21:04:08 -0700

    happy with progress and logging out

--- client/src/components/Itinerary.jsx
+++ client/src/components/Itinerary.jsx
@@ -33,38 +33,41 @@ export default function Itinerary({ wineries, somm }) {
   //i want the ability for user to rewrite whatever they want
   // must be logged in to view saved wishlist
 
+  // prefer winery to open to the sied instead of going to a new tab. To do.
+
   return (
     <>
-      <h4 onClick={handleClick}> My first time in Paso Robles Itineray</h4>
-      <input
-        type="text"
-        placeholder="Where to?"
-        onChange={(e) => {
-          handleChange(e);
-        }}
-      />
-      <button onClick={handleClick}>Add</button>
-      <h3>First Stop:</h3>
-      <p id="firstStop"></p>
-      <input type="time" />
-      Reservation?
-      <span>
-        {" "}
-        <input type="checkbox" />
-      </span>
-      <button onClick={handleAdd}>Add more stops</button>
-      <h4>Saved Spots:</h4>
-      {savedWineries.map((saved) => {
-        return (
-          <p
-            onClick={() => {
-              navigate(`/${saved.winery_id}`);
-            }}
-          >
-            {saved.name}
-          </p>
-        );
-      })}
+      <div id="itinerarySheet">
+        <div>
+          <h4 onClick={handleClick}> My first time in Paso Robles Itinerary</h4>
+          <div id="flexRow">
+            <input
+              type="text"
+              placeholder="Where to?"
+              onChange={(e) => {
+                handleChange(e);
+              }}
+            />
+            <input type="time" />
+            Reservation? <input type="checkbox" />
+          </div>
+          <button onClick={handleAdd}>+</button>
+        </div>
+        <div>
+          <h4>Wishlist:</h4>
+          {savedWineries.map((saved) => {
+            return (
+              <p
+                onDoubleClick={() => {
+                  navigate(`/${saved.winery_id}`);
+                }}
+              >
+                {saved.name}
+              </p>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
